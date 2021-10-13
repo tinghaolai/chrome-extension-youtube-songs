c65760c305da442d7b6ffefb7a2c4d77f39917e3
Ting-Hao Lai <analth355@gmail.com>
2021-10-13 23:37:20 +0800

    Install element-ui

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
-
 import Popup from './components/Popup.vue';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
 const app = new Vue({
     el: '#app',
     render: createElement => createElement(Popup)
